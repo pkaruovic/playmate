ff1ed1f3d90bf4335fd77584bd4e7ebf9a60c6b1
pkaruovic <pkaruovic@gmail.com>
2019-09-21 11:37:19 +0200

    User#display_name implemented

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   has_many :posts
 
   validates :first_name, :last_name, presence: true
+
+  def display_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,12 @@ describe User, type: :model do
   it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
+
+  describe "#display_name" do
+    it "is first name and last name" do
+      user = create(:user, first_name: "Igor", last_name: "Pantovic")
+
+      expect(user.display_name).to eq "Igor Pantovic"
+    end
+  end
 end
