599f7ef0cd9491511f996387151b1e414961d6b4
pkaruovic <pkaruovic@gmail.com>
2019-09-21 15:42:17 +0200

    Add posts pagination

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,6 @@ class HomesController < ApplicationController
   before_action :require_login
 
   def index
-    @posts = Post.by_date
+    @posts = Post.by_date.page(params[:page]).per(10)
   end
 end
