e0e7d68f787cb16d1a03a085aeda39a168f55469
pkaruovic <pkaruovic@gmail.com>
2019-09-21 12:26:42 +0200

    Implement user updates post feature

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,12 +11,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
   def create
     @post = current_user.posts.new(post_params)
     if @post.save
       redirect_to @post
     else
       render "new"
+    end
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      flash[:note] = "Post successfuly updated"
+      render "edit"
+    else
+      render "edit"
     end
   end
 
---
+++ spec/features/user_updates_post_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+feature "User updates post" do
+  scenario "with valid attributes" do
+    user = create(:user)
+    post = create(:post, user: user)
+    date = 3.days.from_now.to_date.iso8601
+
+    visit edit_post_path(post, as: user)
+    fill_in "Description", with: "Hello there!"
+    fill_in "City", with: "Belgrade"
+    fill_in "Date", with: date
+    select "beginner", from: "Skill level"
+    click_button "Update"
+
+    expect(page).to have_field "Description", with: "Hello there!"
+    expect(page).to have_field "City", with: "Belgrade"
+    expect(page).to have_field "Date", with: date
+    expect(page).to have_field "Skill level", with: "beginner"
+  end
+end
