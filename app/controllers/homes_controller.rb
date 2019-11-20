a640f0c7ed0ae5e05ddeb4cb3e1b8e2428d176ef
pkaruovic <pkaruovic@gmail.com>
2019-11-20 11:22:04 +0100

    HomesController - preload post owner and interested users

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,6 +4,7 @@ class HomesController < ApplicationController
   before_action :require_login
 
   def show
-    @posts = Post.active.by_date.page(params[:page]).per(10)
+    @posts = Post.includes(:user, :interested_users)
+      .active.by_date.page(params[:page]).per(10)
   end
 end
