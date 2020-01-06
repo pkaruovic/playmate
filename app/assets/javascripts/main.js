8bab69379d7a4a83e2d5c3ebf485c112165a7e3d
pkaruovic <pkaruovic@gmail.com>
2020-01-06 23:40:39 +0100

    Fix join request create/destroy views

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,37 @@
-$(document).on(
-  'turbolinks:load',
-  function hideElementNotVisibleToCurrentUser() {
-    $('[data-visible-to]').each(function() {
-      const visibleTo = this.dataset.visibleTo.split(/\s/);
-      const ownerId = parseInt(this.closest('[data-owner-id]').dataset.ownerId);
+$(document).on('turbolinks:load', function() {
+  hideElementsNotVisibleToCurrentUser();
+  hideElementsNotVisibleOnCurrentPage();
+});
 
-      if (visibleTo.includes('owner') && currentUser.id !== ownerId) {
-        this.remove();
-      }
-    });
-  },
-);
+$(document).on('ajax:send', function(event) {
+  const xhr = event.detail[0];
+  xhr.onload = function() {
+    hideElementsNotVisibleToCurrentUser();
+    hideElementsNotVisibleOnCurrentPage();
+  };
+});
+
+function hideElementsNotVisibleToCurrentUser() {
+  $('[data-visible-to]').each(function() {
+    const visibleTo = this.dataset.visibleTo.split(/\s/);
+    const ownerId = parseInt(this.closest('[data-owner-id]').dataset.ownerId);
+
+    if (visibleTo.includes('owner') && currentUser.id !== ownerId) {
+      this.remove();
+    }
+  });
+}
+
+function hideElementsNotVisibleOnCurrentPage() {
+  $('[data-visible-on]').each(function() {
+    const paths = this.dataset.visibleOn.split(/\s/);
+    const currentPath = window.location.pathname;
+
+    if (!paths.includes(currentPath)) {
+      this.remove();
+    }
+  });
+}
 
 function debounce(func, wait, immediate) {
   let timeout;
