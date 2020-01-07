2181ae0792bed28878620991c370ba9dd59ad7bf
pkaruovic <pkaruovic@gmail.com>
2020-01-07 14:01:18 +0100

    Add filters to posts page

--- spec/features/user_searches_for_post_spec.rb
+++ spec/features/user_searches_for_post_spec.rb
@@ -3,56 +3,99 @@
 require "rails_helper"
 
 feature "user searches for post", type: :feature do
-  scenario "by text", js: true do
-    user = create(:user)
-    first_post = create(:post, description: "Hello there!", archived: false)
-    second_post = create(:post, description: "Hola mundo!", archived: false)
-    outdated_post = create(:post, description: "Hello, I'm old", date: 5.days.ago, archived: false)
-    archived_post = create(:post, description: "Hello, I'm archived", archived: true)
+  context "on home page" do
+    scenario "by text", js: true do
+      user = create(:user)
+      first_post = create(:post, description: "Hello there!", archived: false)
+      second_post = create(:post, description: "Hola mundo!", archived: false)
+      outdated_post = create(:post, description: "Hello, I'm old", date: 5.days.ago, archived: false)
+      archived_post = create(:post, description: "Hello, I'm archived", archived: true)
 
-    visit root_path(as: user)
-    fill_in "query", with: "hello"
+      visit root_path(as: user)
+      fill_in "query", with: "hello"
 
-    expect(page).not_to have_post second_post
-    expect(page).not_to have_post outdated_post
-    expect(page).not_to have_post archived_post
-    # if we set this expectation as first it may be false positive
-    # because first post already exists on the page and search ajax may not be resolved yet
-    expect(page).to have_post first_post
+      expect(page).not_to have_post second_post
+      expect(page).not_to have_post outdated_post
+      expect(page).not_to have_post archived_post
+      # if we set this expectation as first it may be false positive
+      # because first post already exists on the page and search ajax may not be resolved yet
+      expect(page).to have_post first_post
+    end
+
+    scenario "by game type", js: true do
+      user = create(:user)
+      first_post = create(:post, game_type: "board game", archived: false)
+      second_post = create(:post, game_type: "sport", archived: false)
+      outdated_post = create(:post, game_type: "board game", date: 5.days.ago, archived: false)
+      archived_post = create(:post, game_type: "board game", archived: true)
+
+      visit root_path(as: user)
+      select "Board game", from: "game_type", visible: false
+
+      expect(page).not_to have_post second_post
+      expect(page).not_to have_post outdated_post
+      expect(page).not_to have_post archived_post
+      # if we set this expectation as first it may be false positive
+      # because first post already exists on the page and search ajax may not be resolved yet
+      expect(page).to have_post first_post
+    end
+
+    scenario "by date", js: true do
+      user = create(:user)
+      first_post = create(:post, date: next_month_date(18), archived: false)
+      second_post = create(:post, date: next_month_date(20), archived: false)
+      archived_post = create(:post, date: next_month_date(20), archived: true)
+
+      visit root_path(as: user)
+      open_date_picker
+      open_next_month
+      select_date_range "19", "21"
+
+      expect(page).not_to have_post first_post
+      expect(page).not_to have_post archived_post
+      expect(page).to have_post second_post
+    end
+
   end
 
-  scenario "by game type", js: true do
-    user = create(:user)
-    first_post = create(:post, game_type: "board game", archived: false)
-    second_post = create(:post, game_type: "sport", archived: false)
-    outdated_post = create(:post, game_type: "board game", date: 5.days.ago, archived: false)
-    archived_post = create(:post, game_type: "board game", archived: true)
+  context "on posts page" do
+    scenario "by text", js: true do
+      user = create(:user)
+      first_post = create(:post, user: user, description: "Hello there!", archived: false)
+      second_post = create(:post, user: user, description: "Hola mundo!", archived: false)
+      outdated_post = create(:post, user: user, description: "Hello, I'm old", date: 5.days.ago, archived: false)
+      archived_post = create(:post, user: user, description: "Hello, I'm archived", archived: true)
+      another_user_post = create(:post, description: "Hello, i belong to another user!", archived: false)
 
-    visit root_path(as: user)
-    select "Board game", from: "game_type", visible: false
+      visit posts_path(as: user)
+      select "Active", from: "archived", visible: false
+      fill_in "query", with: "hello"
 
-    expect(page).not_to have_post second_post
-    expect(page).not_to have_post outdated_post
-    expect(page).not_to have_post archived_post
-    # if we set this expectation as first it may be false positive
-    # because first post already exists on the page and search ajax may not be resolved yet
-    expect(page).to have_post first_post
-  end
+      expect(page).not_to have_post another_user_post
+      expect(page).not_to have_post second_post
+      expect(page).not_to have_post archived_post
+      # if we set this expectation as first it may be false positive
+      # because first post already exists on the page and search ajax may not be resolved yet
+      expect(page).to have_post first_post
+      expect(page).to have_post outdated_post
+    end
 
-  scenario "by date", js: true do
-    user = create(:user)
-    first_post = create(:post, date: next_month_date(18), archived: false)
-    second_post = create(:post, date: next_month_date(20), archived: false)
-    archived_post = create(:post, date: next_month_date(20), archived: true)
+    scenario "by archived status", js: true do
+      user = create(:user)
+      archived_post = create(:post, user: user, archived: true)
+      active_post = create(:post, user: user, archived: false)
 
-    visit root_path(as: user)
-    open_date_picker
-    open_next_month
-    select_date_range "19", "21"
+      visit posts_path(as: user)
+      select "Archived", from: "archived", visible: false
 
-    expect(page).not_to have_post first_post
-    expect(page).not_to have_post archived_post
-    expect(page).to have_post second_post
+      expect(page).not_to have_post active_post
+      expect(page).to have_post archived_post
+
+      select "Active", from: "archived", visible: false
+
+      expect(page).not_to have_post archived_post
+      expect(page).to have_post active_post
+    end
   end
 
   def open_date_picker
